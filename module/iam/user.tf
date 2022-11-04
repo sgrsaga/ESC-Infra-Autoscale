8535439875501adf828b26868c3811c7c6db20b6
sgrsaga <sgrsagara@gmal.com>
2022-11-05 01:00:31 +0530

    without password

--- module/iam/user.tf
+++ module/iam/user.tf
@@ -5,8 +5,16 @@ resource "aws_iam_user" "profbob" {
   force_destroy = true
 }
 
+
 resource "aws_iam_user_login_profile" "profbob_profile" {
   user    = aws_iam_user.profbob.name
   # If the user has username in keybase we can use that to generate password
   #pgp_key = "keybase:sgrsagara"
+  lifecycle {
+    ignore_changes = [
+      password_length,
+      password_reset_required,
+      pgp_key,
+    ]
+  }
 }

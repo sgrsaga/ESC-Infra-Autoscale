bfc5b68cf00b8a18b6d9480d91ca9343abfd4c29
sgrsaga <sgrsagara@gmal.com>
2022-11-05 01:12:55 +0530

    removed aws_iam_user_login_profile

--- module/iam/user.tf
+++ module/iam/user.tf
@@ -5,10 +5,11 @@ resource "aws_iam_user" "profbob" {
   force_destroy = true
 }
 
-
+/*
 resource "aws_iam_user_login_profile" "profbob_profile" {
   user    = aws_iam_user.profbob.name
   # If the user has username in keybase we can use that to generate password
   pgp_key = "VmVyeUNvb2xBV1MyMiMj"
 
 }
+*/

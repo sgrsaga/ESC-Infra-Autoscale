de27b4cba2074c31b112d9a8cd0a78dfd019d1ea
sgrsaga <sgrsagara@gmal.com>
2022-11-05 01:07:01 +0530

    base64 encaded password

--- module/iam/user.tf
+++ module/iam/user.tf
@@ -9,12 +9,6 @@ resource "aws_iam_user" "profbob" {
 resource "aws_iam_user_login_profile" "profbob_profile" {
   user    = aws_iam_user.profbob.name
   # If the user has username in keybase we can use that to generate password
-  #pgp_key = "keybase:sgrsagara"
-  lifecycle {
-    ignore_changes = [
-      password_length,
-      password_reset_required,
-      pgp_key,
-    ]
-  }
+  pgp_key = "VmVyeUNvb2xBV1MyMiMj"
+
 }

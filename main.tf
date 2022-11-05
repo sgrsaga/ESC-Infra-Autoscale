221273745d913ba37c69a3f814053bf8c557cdc4
sgrsaga <sgrsagara@gmal.com>
2022-11-05 22:00:00 +0530

    vpc_security_group_ids = [module.network.private_security_group]

--- main.tf
+++ main.tf
@@ -50,7 +50,6 @@ module "pg_database" {
   source = "./module/rds"
   db_identifier = var.db_identifier
   vpc_id = module.main_network.vpc_id
-  vpc_security_group_ids = [module.network.private_security_group]
   db_name = var.db_name
   depends_on = [module.network] 
 }

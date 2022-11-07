b8aa654204fd96685703a42dd09f84fbd492a734
sgrsaga <sgrsagara@gmal.com>
2022-11-07 21:27:39 +0530

    Re-run with new user_data config for ecs and target group health check

--- module/ecs/ecs.tf
+++ module/ecs/ecs.tf
@@ -37,6 +37,13 @@ resource "aws_lb_target_group" "ecs_alb_tg" {
   port     = 8000
   protocol = "HTTP"
   vpc_id   = var.vpc_id
+  health_check {
+    healthy_threshold   = "5"
+    interval            = "30"
+    unhealthy_threshold = "2"
+    timeout             = "20"
+    path                = "/"
+  }
 }
 
 # Links ALB to TG with lister rule

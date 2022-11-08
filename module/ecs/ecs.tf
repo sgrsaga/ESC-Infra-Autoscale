60bc1c1eeb010e91ee76417606d1c9197e1685a1
sgrsaga <sgrsagara@gmal.com>
2022-11-08 21:18:12 +0530

    Added HTTPS redirect for aws_lb_listener      redirect {       port = 443       protocol = "HTTPS"       status_code = "HTTP_301"     }

--- module/ecs/ecs.tf
+++ module/ecs/ecs.tf
@@ -73,8 +73,14 @@ resource "aws_lb_listener" "alb_to_tg" {
   port = 80
   protocol = "HTTP"
   default_action {
-    target_group_arn = aws_lb_target_group.ecs_alb_tg.id
-    type = "forward"
+    #target_group_arn = aws_lb_target_group.ecs_alb_tg.id
+    type = "redirect"
+
+    redirect {
+      port = 443
+      protocol = "HTTPS"
+      status_code = "HTTP_301"
+    }
   }
 }
 

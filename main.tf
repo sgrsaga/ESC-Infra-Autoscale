fdf49a07a16e1af34897b51b8e944b550b9a6915
sgrsaga <sgrsagara@gmal.com>
2022-11-10 17:03:09 +0530

    Removed additiona provider

--- main.tf
+++ main.tf
@@ -22,11 +22,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-## For AWS Billing - to set blling alarms
-provider "aws" {
-  region = "us-east-1"
-  alias = "billing"
-}
 
 ## 1. Call IAM user create module to create user profile for profbob
 module "iam" {

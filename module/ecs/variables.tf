aa45827ccd5bf08d78bf76dce10857764b44a8e1
sgrsaga <sgrsagara@gmal.com>
2022-11-10 23:17:48 +0530

    add new variables for scaling options

--- module/ecs/variables.tf
+++ module/ecs/variables.tf
@@ -2,8 +2,23 @@
 variable "vpc_id" {
     type = string  
 }
-/*
-variable "target_group_arn" {
-    type = string  
+
+## MAX Running task count
+variable "max_tasks" {
+    type = number  
 }
-*/
+
+## MIN Running task count
+variable "min_tasks" {
+    type = number  
+}
+
+## EC2 AutoScaling AVG CPU threshold 
+variable "asg_avg_cpu_target" {
+    type = number  
+}
+
+## ECS Task AutoScaling AVG CPU threshold 
+variable "ecs_task_avg_cpu_target" {
+    type = number  
+}

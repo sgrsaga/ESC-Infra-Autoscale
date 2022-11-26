bf8be79d81e30aee44e43fa32dd2a8d46cb666c8
sgrsaga <sgrsagara@gmail.com>
2022-11-26 21:54:14 +0530

    snap

--- module/iam/user.tf
+++ module/iam/user.tf
@@ -1,31 +1,4 @@
 ## full read only access
-/*
-resource "aws_iam_role" "ReadOnlyFull" {
-  name = "ReadOnlyFull"
-
-  assume_role_policy = <<EOF
-{
-    "Version": "2008-10-17",
-    "Statement": [
-        {
-            "Sid": "",
-            "Effect": "Allow",
-            "Principal": {
-                "Service": "ecs-tasks.amazonaws.com"
-            },
-            "Action": "sts:AssumeRole"
-        }
-    ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy_attachment" "ReadOnlyFullAttach" {
-  role       = aws_iam_role.ReadOnlyFull.name
-  policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
-}
-*/
-
 ## This module is to create IAM user for GUI Access
 resource "aws_iam_user" "profbob" {
   name          = var.username
@@ -40,25 +13,9 @@ resource "random_string" "sm_postfix_prof"{
   override_special = "-"
 }
 
-/*
-# Create Instance Profile for Proffesor
-resource "aws_iam_instance_profile" "profbob" {
-  name = "profbob-${random_string.sm_postfix_prof.result}"
-  role = aws_iam_role.profbob.name
-}
-*/
-
 resource "aws_iam_policy_attachment" "profbob-attach" {
   name       = "profbob-attach"
   users      = [aws_iam_user.profbob.name]
   policy_arn =  "arn:aws:iam::aws:policy/ReadOnlyAccess"
 }
 
-/*
-resource "aws_iam_user_login_profile" "profbob_profile" {
-  user    = aws_iam_user.profbob.name
-  # If the user has username in keybase we can use that to generate password
-  pgp_key = "VmVyeUNvb2xBV1MyMiMj"
-
-}
-*/
